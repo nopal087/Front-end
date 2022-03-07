332a7ce2fa565e4dec591d3cd8d17fd076bbfc82
Muhammad Naufal Faruq <naufalfaruq285@gmail.com>
2022-03-07 21:42:26 +0700

    deploy

---
+++ precache-manifest.9ad5df3d4b797e03e7a7e0585ed6afca.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f97e8457dc830d93d1b",
+    "url": "/Front-end/css/about.5e0c599a.css"
+  },
+  {
+    "revision": "cc3a6ca957b5121978ff",
+    "url": "/Front-end/css/app.cca52444.css"
+  },
+  {
+    "revision": "dbf3276a9b059aca8224",
+    "url": "/Front-end/css/chunk-vendors.cfca7b2e.css"
+  },
+  {
+    "revision": "c805a55ee4bb00e22d29",
+    "url": "/Front-end/css/contact.79231669.css"
+  },
+  {
+    "revision": "b34539dee5889307825992bfddccf674",
+    "url": "/Front-end/img/foto.b34539de.jpg"
+  },
+  {
+    "revision": "ebb37e50fdd0e31510fca0a8e07d08a4",
+    "url": "/Front-end/img/foto3.ebb37e50.jpg"
+  },
+  {
+    "revision": "48f2a50314a2f7efb982539cfaaf0452",
+    "url": "/Front-end/img/vueart.48f2a503.png"
+  },
+  {
+    "revision": "917c02b5b50903b974216ea1d6cb62fe",
+    "url": "/Front-end/img/web.917c02b5.jpg"
+  },
+  {
+    "revision": "c77f66d6f72f49e5e6bc534d428ec329",
+    "url": "/Front-end/index.html"
+  },
+  {
+    "revision": "2f97e8457dc830d93d1b",
+    "url": "/Front-end/js/about.ea529876.js"
+  },
+  {
+    "revision": "cc3a6ca957b5121978ff",
+    "url": "/Front-end/js/app.c7dc6134.js"
+  },
+  {
+    "revision": "dbf3276a9b059aca8224",
+    "url": "/Front-end/js/chunk-vendors.24295fe2.js"
+  },
+  {
+    "revision": "c805a55ee4bb00e22d29",
+    "url": "/Front-end/js/contact.b0463230.js"
+  },
+  {
+    "revision": "4de6907c6b09a30484cf",
+    "url": "/Front-end/js/project.75589b2d.js"
+  },
+  {
+    "revision": "f77895b831487312a1b4c755d3f2199a",
+    "url": "/Front-end/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Front-end/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Front-end/precache-manifest.9ad5df3d4b797e03e7a7e0585ed6afca.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "portofolio1"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
